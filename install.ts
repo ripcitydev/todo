de765dffc6969a1b6092dbbc779e473d88fa8b14
Casey Olivier <casey.olivier@nike.com>
2020-02-17 10:33:34 -0800

    Added binary script.

--- install.ts
+++ install.ts
@@ -1,6 +1,8 @@
 const fs = require('fs');
 
-const config = JSON.parse(fs.readFileSync('./package.json'));
+const config = JSON.parse(fs.readFileSync(`${process.env.INIT_CWD}/package.json`));
+
+config['scripts']['todo'] = './node_modules/.bin/todo';
 
 config['todo'] = {
     includes: ['.'],
@@ -8,6 +10,6 @@ config['todo'] = {
     extensions: ['*']
 }
 
-fs.writeFileSync('./package.json', JSON.stringify(config, null, '  '));
+fs.writeFileSync(`${process.env.INIT_CWD}/package.json`, JSON.stringify(config, null, '  '));
 
 //console.log(config);
